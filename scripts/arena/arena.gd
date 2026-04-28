ef05bb8ff6e974d310e5d41973b2cddd2edfd05b
Neyro Caraig <neyrocaraig@gmail.com>
2026-04-28 08:43:22 +0800

    feat: added initial arena scene

---
+++ scripts/arena/arena.gd
@@ -0,0 +1,17 @@
+class_name Arena
+extends Node2D
+
+var player_scene: PackedScene = preload("res://scenes/player/player.tscn")
+var player_instance: Player
+
+func _ready() -> void:
+	_spawn_player()
+
+func _spawn_player() -> void:
+	player_instance = player_scene.instantiate() as Player
+	# Spawn in the exact center of the arena
+	player_instance.global_position = Vector2.ZERO 
+	add_child(player_instance)
+	
+	# The PhantomCamera2D on the player will automatically detect the MainCamera's 
+	# PhantomCameraHost and take control of it smoothly.
